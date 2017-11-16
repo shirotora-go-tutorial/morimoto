00d1152b2d707d11b8836e09d3475bcec7cb1f47
Your Name <nozomotoitech@gmail.com>
2017-11-16 14:01:35 +0900

    finished 3.3.5.1

--- go_web_oreilly/chat/avatar.go
+++ go_web_oreilly/chat/avatar.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io/ioutil"
+	"path/filepath"
 )
 
 var ErrNoAvatarURL = errors.New("chat: can t get avatar url")
@@ -37,7 +39,17 @@ var UseFileSystemAvatar FileSystemAvatar
 func (_ FileSystemAvatar) GetAvatarURL(c *client)(string, error){
 	if userid, ok := c.userData["userid"]; ok {
 		if useridStr, ok := userid.(string); ok{
-			return "/avatars/" + useridStr + ".jpg", nil
+			if files, err := ioutil.ReadDir(); err == nil{
+				for _, file := range files{
+					if file.IsDir(){
+						continue
+					}
+					if match, _ := filepath.Match(useridStr + "*", file.Name());
+						match {
+							return "/avatars/" + file.Name(), nil
+					}
+				}
+			}
 		}
 	}
 	return "", ErrNoAvatarURL
